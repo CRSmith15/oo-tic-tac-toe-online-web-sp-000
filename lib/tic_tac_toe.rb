a6c80e5f4b007d85c9a4882f307522eccbb0e036
Chance Smith <chancesmith97@hotmail.com>
2020-02-29 23:05:13 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,6 +26,10 @@ class TicTacToe
   def input_to_index(input)
     input.to_i - 1
   end
+  
+  def move(index, token = "X")
+    @board[index] = token
+  end
 
   def play
     @board = Array.new(9, " ")
