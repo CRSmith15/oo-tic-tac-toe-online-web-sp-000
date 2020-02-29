fd1cd8e2dfd289dacc86b47b5eec82d57a282cfa
Chance Smith <chancesmith97@hotmail.com>
2020-02-29 23:17:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -83,7 +83,14 @@ class TicTacToe
     won? || draw?
   end
   
-  
+  def winner
+    if won?
+      @board[won?[0]] == "X" ? "X" : "O"
+    else
+      nil
+    end
+  end
+
 
   def play
     @board = Array.new(9, " ")
